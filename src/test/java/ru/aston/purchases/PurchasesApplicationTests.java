c8c8473d61bb27d9be8b2eb4b85356bf106ff7de
Vladislav <vladlovt@yandex.ru>
2024-05-15 01:06:26 +0300

    feat: начало

---
+++ src/test/java/ru/aston/purchases/PurchasesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.aston.purchases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PurchasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
